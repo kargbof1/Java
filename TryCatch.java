d29551b1eaa4b28b7e059fcb2699db726739b8e4
Fatmata Kargbo <98076814+kargbof1@users.noreply.github.com>
2023-01-08 16:51:21 -0700

    Create TryCatch.java

---
+++ TryCatch.java
@@ -0,0 +1,20 @@
+public class TryCatch {
+    public static void main(String[] args)
+    {
+    int a = 1;
+    int b = 0;
+    int c = 0;
+         
+    try {
+        c= a/b;
+        
+    } catch (Exception e) {
+        System.out.println("it broke");
+
+        // TODO: handle exception
+    }
+
+       
+    }
+    
+}
