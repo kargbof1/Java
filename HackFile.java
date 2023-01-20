fc9f7013e7a4c70608994be8c7492296da7e023f
Fatmata Kargbo <98076814+kargbof1@users.noreply.github.com>
2023-01-20 14:08:01 -0700

    Create HackFile.java

---
+++ HackFile.java
@@ -0,0 +1,8 @@
+public class HackerRankSolution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        String hello = "Hello, World." + " \nHello, Java.";
+        System.out.println(hello);
+    }
+}
