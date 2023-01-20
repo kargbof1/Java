80a395d693f5d01706926c6a7e109c43ae805020
Fatmata Kargbo <98076814+kargbof1@users.noreply.github.com>
2023-01-20 14:12:23 -0700

    Update HackFile.java

--- HackFile.java
+++ HackFile.java
@@ -6,3 +6,15 @@ public class HackerRankSolution {
         System.out.println(hello);
     }
 }
+
+public class ScannerSolution {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+String myString = scanner.next();
+int myInt = scanner.nextInt();
+scanner.close();
+
+System.out.println("myString is: " + myString);
+System.out.println("myInt is: " + myInt);
+    }
+}
